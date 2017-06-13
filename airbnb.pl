e0608f122fa940f2e44e9971320d110e5e1b3b82
JJ Merelo <jjmerelo@gmail.com>
2017-06-13 18:43:16 +0200

    Dealing with errors

--- airbnb.pl
+++ airbnb.pl
@@ -20,27 +20,35 @@ say "ID, ", join(", ", @columns);
 for my $u ( @urls ) {
   my ($id) = ($u =~ /(\d+)/);
   my $page = get( $u );
-  my ($json_chunk) = ($page =~ /script type="application.json" data-hypernova-key="p3indexbundlejs" data-hypernova-id="\S+"><!--(.+)-->/);
-  my $u_data = utf8::is_utf8($json_chunk) ? Encode::encode_utf8($json_chunk) : $json_chunk;
-  my $airbnb_data = decode_json $u_data;
-  my $space_data = $airbnb_data->{'bootstrapData'}{'listing'}{'space_interface'};
-  for my $d (@$space_data) {
-    $data{$id}{$d->{'label'}} = $d->{'value'}
-  }
-  my $price_data = $airbnb_data->{'bootstrapData'}{'listing'}{'price_interface'};
-  $data{$id}{'limpieza'} = $price_data->{'cleaning_fee'}{'value'};
-  $data{$id}{'extra'} = $price_data->{'extra_people'}{'value'};
-  $data{$id}{'finde'} = $price_data->{'weekend_price'}{'value'};
-
-  #Print now
   print "$id";
-  for my $c ( @columns ) {
-    if (!$data{$id}{"$c"}) {
-      print ", ";
-      next;
+  if ( $page ) {
+    my ($json_chunk) = ($page =~ /script type="application.json" data-hypernova-key="p3indexbundlejs" data-hypernova-id="\S+"><!--(.+)-->/);
+    if (!$json_chunk) {
+      print ", No longer available,,,,,, ";
+    } else {
+      my $u_data = utf8::is_utf8($json_chunk) ? Encode::encode_utf8($json_chunk) : $json_chunk;
+      my $airbnb_data = decode_json $u_data;
+      my $space_data = $airbnb_data->{'bootstrapData'}{'listing'}{'space_interface'};
+      for my $d (@$space_data) {
+	$data{$id}{$d->{'label'}} = $d->{'value'}
+      }
+      my $price_data = $airbnb_data->{'bootstrapData'}{'listing'}{'price_interface'};
+      $data{$id}{'limpieza'} = $price_data->{'cleaning_fee'}{'value'};
+      $data{$id}{'extra'} = $price_data->{'extra_people'}{'value'};
+      $data{$id}{'finde'} = $price_data->{'weekend_price'}{'value'};
+      
+      #Print now
+      for my $c ( @columns ) {
+	if (!$data{$id}{"$c"}) {
+	  print ", ";
+	  next;
+	}
+	my $u_data = utf8::is_utf8($data{$id}{"$c"}) ? Encode::encode_utf8($data{$id}{"$c"}) : $data{$id}{"$c"};
+	print ", $u_data";
+      }
     }
-    my $u_data = utf8::is_utf8($data{$id}{"$c"}) ? Encode::encode_utf8($data{$id}{"$c"}) : $data{$id}{"$c"};
-    print ", $u_data";
+  } else {
+      print "Error,,,,,,";
   }
   print "\n";
 }
